faad84891419850186afd96e393ba4db50fd3de9
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2024-12-16 03:20:05 +0530

    updated pages seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+
 import { Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '../components/theme-provider'
@@ -6,10 +6,26 @@ import { Navigation } from '@/components/navigation'
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Personal portfolio website',
-}
+export const metadata = {
+  title: "Aman Raj - Full Stack Developer Portfolio",
+  description:
+    "Explore the portfolio of Aman Raj, a full-stack developer specializing in building scalable web applications. Discover projects, skills, and opportunities for collaboration.",
+  keywords:
+    "full stack developer, web applications, Next.js, portfolio, Aman Raj",
+  author: "Aman Raj",
+  openGraph: {
+    title: "Aman Raj - Full Stack Developer Portfolio",
+    description: "Explore my work and projects as a full-stack developer.",
+    url: "https://www.aman-raj.xyz/",
+    type: "website",
+    image: "https://www.github.com/huamanraj.png", 
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    site: "huamanraj",
+  },
+};
+
 
 export default function RootLayout({
   children,
